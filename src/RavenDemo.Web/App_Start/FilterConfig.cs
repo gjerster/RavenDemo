f8ebae1d440977863b25b84cd6aaa95e7b2dff0e
Henrik Gjerster Hjarding <gjerster@gmail.com>
2013-03-04 11:15:06 +0100

    Initial commit

---
+++ src/RavenDemo.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace RavenDemo.Web.App_Start
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
